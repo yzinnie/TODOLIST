e255134fa288bdde65eb6ff3b1eef18da60d897f
Jinnie <yyjh222@gmail.com>
2023-10-25 16:15:39 +0900

    Update background img
    img 파일 수정

--- js/backgroud.js
+++ js/backgroud.js
@@ -1,4 +1,4 @@
-const images = ['0.jpg', '1.jpg', '2.jpg', '3.jpg'];
+const images = ['1.jpg', '2.jpg'];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement('img');
