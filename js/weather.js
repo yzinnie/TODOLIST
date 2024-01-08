e939581543801e152c605115a645002d3f465b9c
yzinnie <yyjh222@gmail.com>
2024-01-08 17:36:29 +0900

    UPDATE files

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,6 @@ const API_KEY = '7840db3f6ea08d7a7a2192e1fa39d4dc';
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  console.log('You live in', lat, lon);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
   fetch(url)
@@ -12,7 +11,7 @@ function onGeoOk(position) {
       const city = document.querySelector('#weather span:first-child');
       const weather = document.querySelector('#weather span:last-child');
 
-      city.innerText = `📍${data.name}`;
+      city.innerText = `${data.name}`;
       weather.innerText = `${data.main.temp}°`;
     });
 }
