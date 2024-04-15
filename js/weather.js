9ec5e1cb041f74b2ccee71c0ab59ed3b9be6c2e8
윤지현 <yyjh222@gmail.com>
2024-04-15 15:24:10 +0900

    deploy test

--- js/weather.js
+++ js/weather.js
@@ -3,22 +3,21 @@ const API_KEY = config.API_KEY;
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
 
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const city = document.querySelector('#weather span:first-child');
       const weather = document.querySelector('#weather span:last-child');
-
+      const temp = Math.trunc(data.main.temp);
       city.innerText = `${data.name}`;
-      weather.innerText = `${data.main.temp}°`;
+      weather.innerText = `${temp}° `;
     });
 }
-// ${data.weather[0].main}
 
 function onGeoError() {
-  alert('위치를 받아오지 못했습니다.');
+  alert('위치를 받아오지 못했습니다. 다시 시도해주세요.');
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
